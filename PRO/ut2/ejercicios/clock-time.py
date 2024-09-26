5bda3c47c2376fe20c19da1f3bb24b699077a117
Antonio navarro hernandez <tillidie9999@gmail.com>
2024-09-26 11:09:10 +0100

    ejer num

---
+++ PRO/ut2/ejercicios/clock-time.py
@@ -0,0 +1,16 @@
+# *********************
+# CONTANDO MILISEGUNDOS
+# *********************
+
+
+def run(hours: int, minutes: int, seconds: int) -> float:
+    hours_to_milliseconds = hours * 3600000 
+    minutes_to_milliseconds = minutes * 60000
+    seconds_to_milliseconds = seconds * 1000
+    time_since_midnight = hours_to_milliseconds + minutes_to_milliseconds + seconds_to_milliseconds
+
+    return time_since_midnight
+
+
+if __name__ == '__main__':
+    run(0, 1, 1)
