3ce30f8eef50e3555de8b7a92a26891a8826fa5a
Antonio Navarro Hernández <145054110+toninavhd@users.noreply.github.com>
2024-10-07 10:50:30 +0100

    ejercicios finde

---
+++ PRO/ut2/ejercicios/strings/count-sheeps.py
@@ -0,0 +1,11 @@
+def run(num_sheeps: int) -> str:
+    sheep = 'sheep...'
+    sleep = num_sheeps * sheep
+    return sleep
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
