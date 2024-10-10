708f56b3779d8dc1fd2b539e941763fc9bd689b0
Antonio Navarro Hernández <145054110+toninavhd@users.noreply.github.com>
2024-10-10 15:33:03 +0100

    Update fstring-format.py

--- PRO/ut2/ejercicios/strings/fstring-format.py
+++ PRO/ut2/ejercicios/strings/fstring-format.py
@@ -1,11 +1,17 @@
 def run(number):
-
-   print(f"{number:.3f}")
-   print(f"{number:.6f}")
-   print(f"{number:8.2f}")
-   print(f"{number:.6e}")
-   print(f"{number:010.4f}")
-   print(f"{number:19.5f}")
+    print(
+        f'{number:.3f}'
+        '\n'
+        f'{number:.6f}'
+        '\n'
+        f'{number:8.2f}'
+        '\n'
+        f'{number:6e}'
+        '\n'
+        f'{number:010.4f}'
+        '\n'
+        f'{number:19.5f}'
+    )
     
 
 
