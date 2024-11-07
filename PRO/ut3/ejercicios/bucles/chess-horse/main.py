2af891eb855ef3e09283670eaaa8b47abc63b3f9
Antonio Navarro Hernández <145054110+toninavhd@users.noreply.github.com>
2024-11-07 15:42:22 +0000

    Update chess-horse main.py

--- PRO/ut3/ejercicios/bucles/chess-horse/main.py
+++ PRO/ut3/ejercicios/bucles/chess-horse/main.py
@@ -1,18 +1,18 @@
 def run(target_x: int, target_y: int) -> int:
-    x = 1
-    y = 1
+    x = 0
+    y = 0
     movements = 0
 
     while x < target_x or y < target_y:
-        if x < target_x:
-            x += 2
-            y += 1
-        elif y < target_y:
-            y += 2
+        if movements % 2 == 0:
             x += 1
+            y += 2
+        else:
+            y += 1
+            x += 2
         movements += 1
-        if target_x < x and target_x < y:
-            movements = -1       
+    if target_x != x or target_x != y:
+        movements = -1       
 
     return movements
 
