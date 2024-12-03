05aa1164d272ab36de26adbc2f3b66b571afde0c
toninavhd <tillidie9999@gmail.com>
2024-12-03 18:38:42 +0000

    tareas bae

--- BAE/tarea_paro/tarea5.sql
+++ BAE/tarea_paro/tarea5.sql
@@ -1,17 +1,16 @@
 USE parobasicas
 GO
-----
+-- Sumar el padrón de las ca con más de 5 provincias para datos del 1/3/2013
 SELECT ca, SUM(padron) AS total_padron
 FROM DatosCompletosTabla
 WHERE fecha = '1/3/2013'
 GROUP BY CA
 HAVING COUNT(provincia)>5
 
-
+-- Sumar el paro de las provincias con 4 islas (usando having) para datos 1/1/2013.
 SELECT SUM(TotalParoRegistrado) AS total_paro
 FROM DatosCompletosTabla
 WHERE fecha = ' 1/1/2013'
 GROUP BY provincia
-HAVING COUNT(isla) = 4
-
+HAVING COUNT(isla) > 4
 ----
--- BAE/tarea_paro/tarea5audiencia.sql
+++ BAE/tarea_paro/tarea5audiencia.sql
@@ -1,15 +1,18 @@
 USE audienciasbasicas
 GO
+-- Suma de audiencia de programas para cada cadena en martes y para las cadenas con tres o menos programas.
 
 SELECT cadena, SUM(Espectadores) AS total_audiencia
 FROM Datosprogramas
-WHERE DATENAME( FechaHora , 2) = 'martes'
+WHERE DATENAME(dw, FechaHora) = 'Martes'
 GROUP BY cadena
 HAVING COUNT(programa) <= 3;
 
+-- Mostrar las cadenas con media de share mayor que 10 en el horario de las 10, 11 y 12 de la mañana.
+
 SELECT cadena
 FROM Datosprogramas
-WHERE FechaHora IN ('10:00:00', '11:00:00', '12:00:00')
+WHERE DATEPART (HOUR,FechaHora) BETWEEN 10 and 12
 GROUP BY cadena
 HAVING AVG(share) > 10;
 
