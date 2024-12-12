62e213623886b24748e6138d5084d2807fc56edd
Antonio Navarro Hernández <145054110+toninavhd@users.noreply.github.com>
2024-12-12 15:48:43 +0000

    Update main.py

--- PRO/ut4/tareas/listas/minmax/main.py
+++ PRO/ut4/tareas/listas/minmax/main.py
@@ -1,7 +1,7 @@
 def run(values: list) -> tuple:
     min_value = max_value = values[0]
 
-    for value in values:
+    for value in values[1:]:
         if value > max_value:
             max_value = value
         elif value < min_value:
