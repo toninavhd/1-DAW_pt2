aad86498529ec01825ab84c6b4b5feb4d339726a
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-01-29 13:01:34 +0000

    tareas ficheros

--- PRO/ut5/tareas/ficheros/avg-temps/main.py
+++ PRO/ut5/tareas/ficheros/avg-temps/main.py
@@ -1,6 +1,13 @@
 def run(input_path: str, output_path: str) -> None:
-    # TODO
-
+    
+    with open(input_path) as input_file:
+        for line in input_file:
+            line = line.split(',')
+        with open(output_path, 'w') as output_file:
+            temps = [int(line) for line in input_file]
+            avg_temp = sum(temps) / len(temps)
+            avg_temp = round(avg_temp, 2)
+            output_file.write(f'{avg_temp}\n')
 
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
