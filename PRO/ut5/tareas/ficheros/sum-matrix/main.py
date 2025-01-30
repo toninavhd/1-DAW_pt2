c06f83e123d0188cb34c6abb2360c41df71ccd22
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-01-30 20:40:47 +0000

    probar en clase

--- PRO/ut5/tareas/ficheros/sum-matrix/main.py
+++ PRO/ut5/tareas/ficheros/sum-matrix/main.py
@@ -1,20 +1,15 @@
 def run(matrix1_path: str, matrix2_path: str, result_path: str) -> None:
-    with open(matrix1_path) as matrix1, open(matrix2_path) as matrix2, open(result_path, 'w', encoding='utf-8') as result:
+    with open(matrix1_path, encoding='utf-8') as matrix1, open(matrix2_path, encoding='utf-8') as matrix2, open(result_path, 'w', encoding='utf-8') as result:
         lines1 = matrix1.readlines()
         lines2 = matrix2.readlines()
 
-        def convert_lines_to_matrix(lines):
-            return [[int(num) for num in line.strip().split()] for line in lines]
+        matrix1_num = [[int(num) for num in line.strip().split()] for line in lines1]
+        matrix2_num = [[int(num) for num in line.strip().split()] for line in lines2]
 
-        matrix1_num = convert_lines_to_matrix(lines1)
-        matrix2_num = convert_lines_to_matrix(lines2)
-
-        result_lines = []
-        for numbers1, numbers2 in zip(matrix1_num, matrix2_num):
-            result_line = ' '.join(str(num1 + num2) for num1, num2 in zip(numbers1, numbers2))
-            result_lines.append(result_line)
-        
-        result.write('\n'.join(result_lines))
+        for m_values1, m_values2 in zip(matrix1_num, matrix2_num):
+            sums = [str(num1 + num2) for num1, num2 in zip(m_values1, m_values2)]
+            result.write(' '.join(sums) + '\n')
+            
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
     import vendor
