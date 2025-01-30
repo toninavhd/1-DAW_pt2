fa38096bd32726cd889ddc1641acf548a2a3af36
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-01-30 09:38:48 +0000

    prueba a pasar los test en clase

--- PRO/ut5/tareas/ficheros/avg-temps/main.py
+++ PRO/ut5/tareas/ficheros/avg-temps/main.py
@@ -2,12 +2,13 @@ def run(input_path: str, output_path: str) -> None:
 
     with open(input_path) as input_file:
         for line in input_file:
-            line = line.split(',')
-        with open(output_path, 'w') as output_file:
-            temps = [int(line) for line in input_file]
-            avg_temp = sum(temps) / len(temps)
-            avg_temp = round(avg_temp, 2)
-            output_file.write(f'{avg_temp}\n')
+            temps = line.split(',')
+            temps = [int(temp) for temp in temps]
+        avg_temp = sum(temps) / 31
+        avg_temp = round(avg_temp, 2)
+        
+    with open(output_path, 'w') as output_file:
+        output_file.write(f'{avg_temp}\n')
 
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
