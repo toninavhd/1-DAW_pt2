bc7fd83212b3625e36cf6ea77b1e6c1ec5aa0d2b
toninavhd <tillidie9999@gmail.com>
2025-01-30 16:14:04 +0000

    ejer

--- PRO/ut5/tareas/ficheros/avg-temps/main.py
+++ PRO/ut5/tareas/ficheros/avg-temps/main.py
@@ -1,16 +1,11 @@
 def run(input_path: str, output_path: str) -> None:
-    sum_temps = 0
-
-    with open(input_path) as f:
-        for line in f:
-            temps = line.split(',')
-            for temp in temps:
-                sum_temps += float(temp)
-            avg_temp = sum_temps / 31
-            avg_temp = round(avg_temp, 2)
-
-    with open(output_path, 'w') as write_file:
-        write_file.write(f'{avg_temp}')
+    with open(input_path) as input_file:
+        with open(output_path, 'w') as output_file:
+            for line in input_file:
+                temps = line.strip().split(',')
+                temps_int = [int(temp) for temp in temps]
+                avg_temp = sum(temps_int) / len(temps_int)
+                output_file.write(f'{avg_temp:.2f}\n')
 
 
 # DO NOT TOUCH THE CODE BELOW
