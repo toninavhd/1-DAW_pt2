6ad04c732e3d5bea35f29d1106cf10d4ad2d6499
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-01-30 20:56:47 +0000

    git commit sv1c1d3

--- PRO/ut5/tareas/ficheros/replace-chars/main.py
+++ PRO/ut5/tareas/ficheros/replace-chars/main.py
@@ -1,21 +1,11 @@
 def run(input_path: str, replacements: str, output_path: str) -> None:
-    with open(input_path) as f:
+    replacement_dict = {pair[0]: pair[1] for pair in replacements.split('|')}
+    
+    with open(input_path) as f, open(output_path, 'w') as out:
         for line in f:
-            line = line.strip('|')
-            for word in line:
-                word = word.swapcase
-
-        with open(output_path, 'w') as w_file:
-            for line in f:
-                for replacement in line:
-                    replacement = replacement.swapcase
-
-        for line in f:
-            line = line.strip('|')
-            for word in line:
-                word = word.swapcase()
-                replacements = word
-
+            for old_char, new_char in replacement_dict.items():
+                line = line.replace(old_char, new_char)
+            out.write(line)
 
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
