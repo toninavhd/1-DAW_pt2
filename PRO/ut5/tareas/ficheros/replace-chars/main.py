80921f1ad494356fd239759982c5239d4840d3be
toninavhd <tillidie9999@gmail.com>
2025-01-30 16:15:42 +0000

    ejer

--- PRO/ut5/tareas/ficheros/replace-chars/main.py
+++ PRO/ut5/tareas/ficheros/replace-chars/main.py
@@ -1,5 +1,20 @@
 def run(input_path: str, replacements: str, output_path: str) -> None:
-    # TODO
+    with open(input_path) as f:
+        for line in f:
+            line = line.strip('|')
+            for word in line:
+                word = word.swapcase
+
+        with open(output_path, 'w') as w_file:
+            for line in f:
+                for replacement in line:
+                    replacement = replacement.swapcase
+
+        for line in f:
+            line = line.strip('|')
+            for word in line:
+                word = word.swapcase()
+                replacements = word
 
 
 # DO NOT TOUCH THE CODE BELOW
