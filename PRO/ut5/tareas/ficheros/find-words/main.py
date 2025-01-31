c71cb6f2a62ff19d9cfe8c7f4cafa6acf6af182f
toninavhd <tillidie9999@gmail.com>
2025-01-31 14:51:45 +0000

    tarea

--- PRO/ut5/tareas/ficheros/find-words/main.py
+++ PRO/ut5/tareas/ficheros/find-words/main.py
@@ -1,18 +1,18 @@
 def run(data_path: str, target_word: str) -> list:
+    DELIMITERS = " .,;:()'¡!"
     matches = []
-    delimiters = " .,;:()'¡!"
-    
+
     with open(data_path) as f:
         for line_num, line in enumerate(f, start=1):
             words = line.split()
             index_offset = 0
             for word in words:
-                clean_word = word.strip(delimiters)
+                clean_word = word.strip(DELIMITERS)
                 if clean_word == target_word:
                     column = line.index(word, index_offset) + 1
                     matches.append((line_num, column))
                 index_offset += len(word) + 1
-                
+
     return matches
 
 
