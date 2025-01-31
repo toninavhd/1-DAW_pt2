d3cca034b4b067dca6727282a436c536a659fd7e
toninavhd <tillidie9999@gmail.com>
2025-01-31 17:03:36 +0000

    tarea parocreada

--- BAE/simulacro_consultas/tareaparo2.sql
+++ BAE/simulacro_consultas/tareaparo2.sql
@@ -1,15 +1,26 @@
 
 --1.- Municipios de la isla de Tenerife
+
 --2.- Paro en la Industria en la Provincia de León en el mes de febrero de 2013
+
 --3.- Mostrar las comunidades autónomas y el nº de habitantes (padrón), ordenándolas de mayor a menor población
+
 --4.- Qué Comunidad Autónoma tiene mayor diferencia entre el paro mujeres en la edad 25-45 y la de mujeres menores de 25, en enero de 2013.
+
 --5.- Comunidades autónomas sin islas
+
 --6.- Crear una vista que muestre el nombre de la comunidad autónoma, el de la provincia y el del municipio, junto al total de paro registrado a fecha 1/3/2013 y al padrón. Usar esta vista para mostrar la dimisión entre paro registrado y padrón para todas las Comunidades autónomas.
+
 --*7.- Dar los nombres de los municipios de la Comunidad autónoma con mayor paro en agricultura (en febrero de 2013).
+
 -- *8.- Número de municipios con más de 200000 habitantes por Comunidad Autónoma en el padrón, sacando todas las Comunidades Autónomas
+
 -- *9.- Municipios con más parados en Servicios entre los habitantes del padrón (Paroservicios/Padron) en febrero de 2013 que la media nacional de la misma división
+
 -- 10.- Indicar para cada Comunidad Autónoma el nº de habitantes por municipio (padrón dividido entre número de municipios), ordenándolas de menor a mayor
+
 --*11.- Diferencia por Comunidad Autónoma entre el nº de parados en marzo de 2013 y en enero de 2013
+
 --*12.- Municipio con más habitantes de cada Comunidad Autónoma.
 
 --(* subconsultas)
