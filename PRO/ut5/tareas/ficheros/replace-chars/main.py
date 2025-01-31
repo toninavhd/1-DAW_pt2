a9df4c5b149904b362e447d733e69a84727a8e6a
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-01-31 10:27:17 +0000

    replace-chars

--- PRO/ut5/tareas/ficheros/replace-chars/main.py
+++ PRO/ut5/tareas/ficheros/replace-chars/main.py
@@ -1,5 +1,9 @@
 def run(input_path: str, replacements: str, output_path: str) -> None:
-    replacement_dict = {pair[0]: pair[1] for pair in replacements.split('|')}
+    replacement_dict = {}
+    for word in replacements.split('|'):
+        old_char = word[0]
+        new_char = word[1]
+        replacement_dict[old_char] = new_char
     
     with open(input_path) as f, open(output_path, 'w') as out:
         for line in f:
