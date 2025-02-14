9d7a2e8ce8d982ba351ec6b799a62fdd2ed86f3a
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-02-14 08:44:46 +0000

    orderbyage

--- PRO/ut6/order-by-age/main.py
+++ PRO/ut6/order-by-age/main.py
@@ -1,5 +1,5 @@
 def order_by_age(people: list[dict]) -> list[dict]:
-    return sorted(people, key=lambda x: x['age'])
+    return sorted(people, key=lambda t: t['age'])
 
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
