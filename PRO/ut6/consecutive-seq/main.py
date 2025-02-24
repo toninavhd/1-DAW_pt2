9941e8ad986ed246366c3c92b5e99f804a529c80
toninavhd <tillidie9999@gmail.com>
2025-02-24 15:56:56 +0000

    esto esta mal hay q arreglarlo

--- PRO/ut6/consecutive-seq/main.py
+++ PRO/ut6/consecutive-seq/main.py
@@ -1,13 +1,11 @@
 def consecutive_seq(values:list, reps:int):
-    rep_counter = 0
+    rep_counter = values[0]
     for value in values:
-        if value == value[0]:
-            rep_counter += 1
-            if reps in rep_counter:
-                return value
+        if value in values[1:]:
+            if value == rep_counter[-1]:
+                rep_counter.append(value)
 
-
-
+        return len(rep_counter)
 # DO NOT TOUCH THE CODE BELOW
 if __name__ == '__main__':
     import vendor
