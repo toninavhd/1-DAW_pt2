99b59393274c458652b469704fc7d84a8e99bd56
toninavhd <tillidie9999@gmail.com>
2025-02-26 16:38:39 +0000

    administracionbasica

--- BAE/indice/administracionbasica.sql
+++ BAE/indice/administracionbasica.sql
@@ -15,17 +15,10 @@ CREATE TABLE empleados (
     cantidadhijos TINYINT CHECK (cantidadhijos >= 0 AND cantidadhijos <= 20),
 
     --e) La sección debe tener entre 3 y 20 caracteres
-    seccion VARCHAR(20) NOT NULL,
+    seccion VARCHAR(20) CHECK != '',
 
     --f) El sueldo debe ser un número decimal con 2 decimales.
     sueldo DECIMAL(6,2),
-    UNIQUE (DNI)
+    UNIQUE (apellidos, nombre)
 );
 
---3 Crear las sentencias para que valide lo siguiente:
---a) El DNI debe ser un número de 8 dígitos.
---b) El nombre y los apellidos deben tener entre 3 y 30 caracteres.
---c) La fecha de nacimiento debe ser menor que la fecha actual.
---d) La cantidad de hijos debe ser un número entero entre 0 y 20.
---e) La sección debe tener entre 3 y 20 caracteres.
---f) El sueldo debe ser un número decimal con 2 decimales.
