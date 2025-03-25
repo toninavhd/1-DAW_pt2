ae2556b718195c126f7a4b38b4d370fb440b9bdd
toninavhd <tillidie9999@gmail.com>
2025-03-25 18:05:05 +0000

    tarea

--- BAE/vistas/tareaprocedimientos_1.sql
+++ BAE/vistas/tareaprocedimientos_1.sql
@@ -5,10 +5,13 @@ create procedure proc_contador
 as
 DECLARE @cadena nvarchar(50);
 DECLARE @contador int;
+SET @cadena = 'Hola Mundo';
 SET @contador = 1;
 WHILE (@contador <= LEN(@cadena))
 BEGIN
 PRINT 'Caracter en la posicion ' + cast(@contador AS varchar) + ': ' + SUBSTRING(@cadena, @contador, 1);
 SET @contador = @contador + 1;
 END
+print 'Final';
+go
 exec proc_contador;
