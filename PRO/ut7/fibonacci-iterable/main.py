b4be11980e7aaa5a6ce8d75b9edb7cecc581a339
toninavhd <tillidie9999@gmail.com>
2025-03-27 16:19:28 +0000

    tarea iterable

--- PRO/ut7/fibonacci-iterable/main.py
+++ PRO/ut7/fibonacci-iterable/main.py
@@ -3,19 +3,13 @@ class Fibonacci:
         self.n = n
         self.a = 0
         self.b = 1
-        self.pointer = 0
 
     def __iter__(self):
-        return self
+        for _ in range(self.n):
+            yield self.a
+            self.a, self.b = self.b, self.a + self.b
+
     
-    def __next__(self):
-        if self.pointer >= self.n:
-            raise StopIteration
-        else:
-            f_value = self.a
-            self.a, self.b = self.b , self.a + self.b
-            self.pointer += 1
-            return f_value
 
     
 def run(n: int) -> list[int]:
