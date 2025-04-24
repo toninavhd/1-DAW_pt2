d79531f421bcfca2cb4ab04537a39c1bca7d83d3
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-04-24 10:56:53 +0100

    twitter

--- PRO/ut9/twitter/main.py
+++ PRO/ut9/twitter/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 import re
 import sqlite3
 
@@ -7,20 +8,19 @@ DB_PATH = 'twitter.db'
 def create_db(db_path: str) -> None:
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-
     sql = """
     CREATE TABLE user (
         id INTEGER PRIMARY KEY,
-        username TEXT UNIQUE,
+        username UNIQUE TEXT,
         password TEXT,
         bio TEXT
-        );
+        )
     CREATE TABLE tweet (
         id INTEGER PRIMARY KEY,
         content TEXT,
         user_id INTEGER REFERENCES user(id),
         retweet_from INTEGER REFERENCES tweet(id)
-        );
+        )
     """
     cur.executescript(sql)
     con.commit()
@@ -43,8 +43,9 @@ class User:
         sql = 'INSERT INTO user (username, password, bio) VALUES (?, ?, ?)'
         data = (self.username, self.password, self.bio)
         User.cur.execute(sql, data)
-        self.user_id = User.cur.lastrowid
         User.con.commit()
+        self.user_id = self.cur.lastrowid 
+        
     
     def login(self, password: str) -> None:
         if self.password == password:
@@ -59,6 +60,13 @@ class User:
         new_tweet.save()
         return new_tweet
     
+    def __repr__(self):
+        return f'{self.username}: {self.bio})'
+    
+    @classmethod
+    def from_db_row(cls, row: sqlite3.Row) -> User:
+        return cls(row['username'], row['password'], row['bio'], row['id'])
+
     @property
     def tweets(self):
         sql = 'SELECT * FROM tweet WHERE user_id = ?'
@@ -71,6 +79,9 @@ class Tweet:
     cur = con.cursor()
 
     def __init__(self, content: str = '', retweet_from: int = 0, tweet_id: int = 0):
+        self.con = sqlite3.connect(DB_PATH)
+        self.con.row_factory = sqlite3.Row
+        self.cur = self.con.cursor()
         self.content = content
         self.retweet_from = retweet_from
         self.tweet_id = tweet_id
@@ -81,12 +92,6 @@ class Tweet:
         self.cur.execute(sql, data)
         self.tweet_id = self.cur.lastrowid
         self.con.commit()
-    
-    def __repr__(self):
-        if self.retweet_from != 0:
-            return f'[RT] {self.content} (id={self.tweet_id})'
-        else:
-            return f'{self.content} (id={self.tweet_id})'
 
     @classmethod
     def from_db_row(cls, row: sqlite3.Row) -> Tweet:
@@ -102,6 +107,16 @@ class Tweet:
     @property
     def is_retweet(self) -> bool:
         return self.retweet_from != 0
+    
+    def __repr__(self):
+        if self.retweet_from != 0:
+            return f'[RT] {self.content} (id={self.tweet_id})'
+        else:
+            return f'{self.content} (id={self.tweet_id})'
+        
+    @classmethod
+    def from_db_row(cls, row: sqlite3.Row) -> Tweet:
+        return cls(row['content'], row['retweet_from'], row['id'])
     
 class Twitter:
     con = sqlite3.connect('twitter.db')
@@ -119,7 +134,6 @@ class Twitter:
         sql = 'SELECT * FROM user WHERE id = ?'
         if match := self.cur.execute(sql, (user_id,)).fetchone():
             return User.from_db_row(match)
-        
         raise TwitterError('User with id {user_id} does not exist')
 
 class TwitterError(Exception):
