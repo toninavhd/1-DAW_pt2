1d0c0fd2236bf07cf55dc0a707389bbffe531a50
Antonio navarro hernandez <tillidie9999@gmail.com>
2025-05-05 19:02:20 +0100

    ut

--- BAE/consultas_basicas_y_agrupaciones/tarea1.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT CA FROM DatosCompletosTabla WHERE CA LIKE '%ia%'
-
-SELECT DISTINCT CA FROM DatosCompletosTabla WHERE CA LIKE '[aeiouáéíóú]%'
-
-SELECT COUNT(DISTINCT Provincia) FROM DatosCompletosTabla WHERE CA = 'Canarias' 
-
-SELECT SUM(Padron) FROM DatosCompletosTabla WHERE CA = 'Canarias'
